2f71f2b68769243564d2e7e6205dace846284b5c
Tatsuya Iwama <gansaibow@gmail.com>
2013-12-09 16:55:31 +0900

    Photoshop Script FolderAlerts
    Photshop のJavascriptでフォルダのパス情報を取得するAlert表示するスクリプト

---
+++ photoshop_scripts/ForderAlerts/ForderAlerts.js
@@ -0,0 +1,14 @@
+// Forderオブジェクトで取得できるフォルダパスをAlert表示する。
+
+alert("ドキュメント\n"+Folder.myDocuments);
+alert("デスクトップ\n"+Folder.desktop);
+alert("ゴミ箱\n"+Folder.trash);
+alert("実行中のアプリのパッケージ\n"+Folder.appPackage);
+alert("OSの種類\n"+Folder.fs);
+alert("システムフォルダ\n"+Folder.system);
+alert("ユーザ別アプリケーションデータの場所\n"+Folder.userData);
+alert("システム用アプリケーションデータの場所\n"+Folder.appData);
+alert("コモンファイルフォルダ\n"+Folder.commonFiles);
+alert("テンプフォルダ\n"+Folder.temp);
+alert("アプリケーションの実行イメージの場所\n"+Folder.startup);
+alert("現在のフォルダ\n"+Folder.current);
